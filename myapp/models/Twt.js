cbfad9f60d4c410703809e0018e190d3b63227bc
Wilhelm Daniel <daniel.wilhelm@etu.unistra.fr>
2015-05-21 13:49:13 +0200

    Modification - ajout de la date

--- myapp/models/Twt.js
+++ myapp/models/Twt.js
@@ -1,9 +1,16 @@
 var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/Mytwr');
+var db = mongoose.connection; //la base de donnée
+db.on('error',console.error);
+db.once('open', function(){ console.log("Connected")});
 var Schema=mongoose.Schema;
 var Twt = new Schema 
     ({
-        user:String;
-        texte:String;
-        categorie:String;
+        auteur:String,
+        titre:String,
+        texte:String,
+        date:String,
     });
-module.exports = mongoose.model("Twt",Twt);
+
+mongoose.connection.close(function(){console.log("Disconnected")});
+module.exports = mongoose.model('Twt',Twt);
