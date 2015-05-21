38b3173159c59dfcfe27bc43483af9bbcfd63005
Wilhelm Daniel <daniel.wilhelm@etu.unistra.fr>
2015-05-21 13:58:52 +0200

    Suppression code inutile

--- myapp/public/javascripts/heure.js
+++ myapp/public/javascripts/heure.js
@@ -4,14 +4,10 @@ function Hour ()
 		h = d.getHours();
 		min = d.getMinutes();
 		s = d.getSeconds();
+        if (h<10) h='0'+h;
+        if (min<10) min = '0'+min;
+        if (s<10) s = '0'+s;
 		txt = h + ": " + min + ": " + s;
 		Calk.innerHTML = txt;
 		setTimeout("Hour()", 1000);
 	}
-var Env=document.getElementById('Envoyer');
-		Env.addEventListener('click',function()
-		{
-			var dat=new Date();
-			document.getElementById('ZoneTexte').innerHTML+='<p>'+document.getElementById('Nom').value+' a écrit à :'+dat.getHours()+':'+dat.getMinutes()+':'+dat.getSeconds()+':</br>'+document.getElementById('Twrer').value+'</p>';
-			document.getElementById('Twrer').value='';
-		},false);
